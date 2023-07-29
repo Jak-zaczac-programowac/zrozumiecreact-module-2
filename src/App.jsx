af4d26900449be8550aa0a7ba6119638e261ca30
filipMaM <filip.mamcarczyk@gmail.com>
2023-07-30 00:32:45 +0200

    Lesson 3 end

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
+
 function App() {
-    function handleClick() {
-        alert("Klik!");
+    const [isSpoilerShown, setIsSpoilerShown] = useState(false);
+    const [isWarningShown, setIsWarningShown] = useState(true);
+
+    function handleShowSpoilerClick() {
+        setIsSpoilerShown(true);
+        console.log("handleClick!");
+        setIsWarningShown(false);
     }
+
+    function handleCloseWarningClick() {
+        setIsWarningShown(false);
+    }
+
+    console.log("<App> - render");
 
     return (
         <>
             <h1>Gwiezdne wojny V</h1>
             <h2>Rok produkcji: 1980</h2>
             <h2>Fabuła</h2>
+            {isWarningShown && (
+                <p>
+                    Uwaga! Opis fabuły zawiera spoilery!
+                    <button onClick={handleCloseWarningClick}>X</button>
+                </p>
+            )}
             <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
-            <button onClick={handleClick}>Pokaż spoiler</button>
-            <p>Vader okazuje się być ojcem Luka.</p>
+            {isSpoilerShown ? (
+                <p>Vader okazuje się być ojcem Luka.</p>
+            ) : (
+                <button onClick={handleShowSpoilerClick}>Pokaż spoiler</button>
+            )}
         </>
     );
 }
