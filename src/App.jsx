b2113494301fbf28fae10971c878288d4552cc74
filipMaM <filip.mamcarczyk@gmail.com>
2023-07-30 22:53:23 +0200

    Lesson 5 end

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function App() {
     const [isSpoilerShown, setIsSpoilerShown] = useState(false);
     const [isWarningShown, setIsWarningShown] = useState(true);
+    const [numberOfLikes, setNumberOfLikes] = useState(50);
 
     function handleShowSpoilerClick() {
         setIsSpoilerShown(true);
@@ -14,12 +15,23 @@ function App() {
         setIsWarningShown(false);
     }
 
+    function handleLikeButtonClick() {
+        setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 1);
+    }
+
+    function handleLoveButtonClick() {
+        setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 3);
+    }
+
     console.log("<App> - render");
 
     return (
         <>
             <h1>Gwiezdne wojny V</h1>
             <h2>Rok produkcji: 1980</h2>
+            <h2>Liczba polubień: {numberOfLikes}</h2>
+            <button onClick={handleLikeButtonClick}>Lubię to!</button>
+            <button onClick={handleLoveButtonClick}>Kocham to!</button>
             <h2>Fabuła</h2>
             {isWarningShown && (
                 <p>
