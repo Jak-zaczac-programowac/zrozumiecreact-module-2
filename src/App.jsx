cc69799b6ea530d98e36fb14b3a835c10f32217c
Paweł Brzozowski <pawel@brzoza.net>
2024-01-08 22:35:15 +0000

    Update 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-    return <></>;
+export default function App() {
+  return (
+    <>
+      <h1>Gwiezdne wojny V</h1>
+      <h2>Rok produkcji: 1980</h2>
+      <h2>Liczba polubień:</h2>
+      <button>Lubię to!</button>
+      <h2>Fabuła</h2>
+      <p>
+        Uwaga! Opis fabuły zawiera spoilery! <button>X</button>
+      </p>
+      <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
+      <button>Pokaż spoiler</button>
+      <p>Vader okazuje się być ojcem Luka.</p>
+    </>
+  );
 }
-
-export default App;
