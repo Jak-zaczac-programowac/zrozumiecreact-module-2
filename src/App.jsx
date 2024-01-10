73e55ae059062abb639d4523c7848b048703c3a0
Paweł Brzozowski <pawel@brzoza.net>
2024-01-10 17:38:45 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
 import { useState } from "react";
 
 export default function App() {
-  const [likes, setLikes] = useState(0);
-  const [spoiler, setSpoiler] = useState(false);
-  const [disclaimer, setDisclaimer] = useState(true);
+  const [numberOfLikes, setNumberOfLikes] = useState(0);
+  const [isSpoilerVisible, setIsSpoilerVisible] = useState(false);
+  const [isDisclaimerVisible, setIsDisclaimerVisible] = useState(true);
 
   const addLike = () => {
-    setLikes((prevState) => prevState + 1);
+    setNumberOfLikes((prevState) => prevState + 1);
   };
-  const showSpoiler = () => {
-    setSpoiler((prevState) => (prevState = true));
+  const handleShowSpoiler = () => {
+    setIsSpoilerVisible((prevState) => (prevState = true));
+    // optional:
+    // setIsDisclaimerVisible((prevState) => (prevState = false));
   };
-  const closeDisclaimer = () => {
-    setDisclaimer((prevState) => (prevState = false));
+  const handleCloseDisclaimer = () => {
+    setIsDisclaimerVisible((prevState) => (prevState = false));
   };
 
   return (
     <>
       <h1>Gwiezdne wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
-      <h2>Liczba polubień: {likes}</h2>
+      <h2>Liczba polubień: {numberOfLikes}</h2>
       <button onClick={addLike}>Lubię to!</button>
       <h2>Fabuła</h2>
-      {!spoiler && disclaimer && (
+      {!isSpoilerVisible && isDisclaimerVisible && (
         <p>
           Uwaga! Opis fabuły zawiera spoilery!{" "}
-          <button onClick={closeDisclaimer}>X</button>
+          <button onClick={handleCloseDisclaimer}>X</button>
         </p>
       )}
       <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
-      {spoiler ? null : <button onClick={showSpoiler}>Pokaż spoiler</button>}
-      {spoiler && <p>Vader okazuje się być ojcem Luka.</p>}
+      {isSpoilerVisible ? null : (
+        <button onClick={handleShowSpoiler}>Pokaż spoiler</button>
+      )}
+      {isSpoilerVisible && <p>Vader okazuje się być ojcem Luka.</p>}
     </>
   );
 }
